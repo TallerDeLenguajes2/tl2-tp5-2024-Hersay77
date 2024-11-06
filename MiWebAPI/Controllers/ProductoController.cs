6448089184f75a37aed17ebfad614b3b25a641b8
hersay77 <hershins7@gmail.com>
2024-11-06 03:32:35 -0300

    Realizado ProductoRepository y ProductoController

--- MiWebAPI/Controllers/ProductoController.cs
+++ MiWebAPI/Controllers/ProductoController.cs
@@ -14,11 +14,47 @@ public class ProductoController : ControllerBase
         productoRepository = new ProductoRepository("Data Source=db/Tienda.db;Cache=Shared"); //Instancia de repositorio producto, envio la cadena de conexion
     }
 
-    [HttpPost("/api/Producto")]
-    public ActionResult<Producto> CrearProducto(Producto producto) 
+    [HttpPost("api/Producto")]
+    public ActionResult<Producto> CrearProducto(Producto producto)
     {
         productoRepository.CrearProducto(producto);
         return Created();
+    }
+
+    [HttpGet("api/Producto")]
+    public ActionResult<List<Producto>> ObtenerProductos()
+    {
+        return Ok(productoRepository.ObtenerProductos());
+    }
+
+    [HttpPut("api/Producto/{Id}")]
+    public ActionResult<List<Producto>> ObtenerProductos(int Id, Producto producto)
+    {
+        productoRepository.ModificarProducto(Id, producto);
+        return Ok();
+    }
+
+    [HttpGet("api/Producto/ObtenerProducto/{Id}")]
+    public ActionResult<Producto> ObtenerProducto(int Id)
+    {
+        Producto productoEncontrado = productoRepository.ObtenerProducto(Id);
+        if (productoEncontrado == null)
+        {
+            return NotFound(); // No se encontro el producto
+        }
+        return Ok(productoEncontrado);
+    }
+
+    [HttpGet("api/Producto/EliminarProducto/{Id}")]
+    public ActionResult EliinarProducto(int Id)
+    {
+        Producto productoEncontrado = productoRepository.ObtenerProducto(Id);
+        if (productoEncontrado == null)
+        {
+            return NotFound(); // No se encontro el producto
+        }
+        productoRepository.EliminarProducto(Id); // Eliminación
+        return NoContent();
     }
 
 }
--- MiWebAPI/Repositorios/ProductoRepository.cs
+++ MiWebAPI/Repositorios/ProductoRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.Data.Sqlite;
 using EspacioProductos;
+using System.Data;
 
 namespace EspacioProductoRepository
 {
@@ -30,26 +31,82 @@ namespace EspacioProductoRepository
 
         public void ModificarProducto(int idProducto, Producto producto)
         {
+            string queryString = @"UPDATE Productos SET Descripcion = @Descripcion, Precio = @Precio WHERE idProducto = @Id"; //actualizo descripcion y precio donde el id del producto sea igual al id recibido
 
+            using (SqliteConnection connection = new SqliteConnection(cadenaDeConexion))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(queryString, connection);
+                command.Parameters.AddWithValue("@Descripcion", producto.Descripcion); //parametrizando consulta
+                command.Parameters.AddWithValue("@Precio", producto.Precio);
+                command.Parameters.AddWithValue("@Id", idProducto);
+                command.ExecuteNonQuery(); //ejecuto el comando actualizando
+                connection.Close();
+            }
         }
 
         public List<Producto> ObtenerProductos()
         {
-            var lista = new List<Producto>();
+            List<Producto> productos = new List<Producto>(); //creo lista de productos donde agregare productos que lea el reader
+            string queryString = @"SELECT * FROM Productos"; //consulta
 
-            return lista;
+            using (SqliteConnection connection = new SqliteConnection(cadenaDeConexion)) //creo conexion
+            {
+                connection.Open(); //abro conexion
+                SqliteCommand command = new SqliteCommand(queryString, connection); //comando con la consulta y conexion
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Producto producto = new Producto();
+                        producto.IdProducto = Convert.ToInt32(reader["idProducto"]);
+                        producto.Descripcion = reader["Descripcion"].ToString();
+                        producto.Precio = Convert.ToInt32(reader["Precio"]);
+                        productos.Add(producto); //agrego el nuevo producto a la lista que se devolvera
+                    }
+                }
+                connection.Close(); //cierro conexion
+            }
+            return productos;
         }
 
         public Producto ObtenerProducto(int idProducto)
         {
-            var producto = new Producto();
+            Producto producto = null;
+            string queryString = @"SELECT * FROM Productos WHERE idProducto = @id ";
 
+            using (SqliteConnection connection = new SqliteConnection(cadenaDeConexion))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(queryString, connection);
+                command.Parameters.AddWithValue("@id", idProducto);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        producto = new Producto();
+                        producto.IdProducto = Convert.ToInt32(reader["idProducto"]);
+                        producto.Descripcion = reader["Descripcion"].ToString();
+                        producto.Precio = Convert.ToInt32(reader["Precio"]);
+                    }
+
+                }
+                connection.Close();
+            }
             return producto;
         }
 
-        public void EliminarProducto(int id)
+        public void EliminarProducto(int idProducto)
         {
-
+            string queryString = @"DELETE  FROM Productos WHERE idProducto = @id ";
+            using (SqliteConnection connection = new SqliteConnection(cadenaDeConexion))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(queryString, connection);
+                command.Parameters.AddWithValue("@id", idProducto);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
         }
     }
 }
